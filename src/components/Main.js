755578bdfaa8dc94ae16b5a5cdc436340ca2da4d
unknown <ramos_victor@outlook.com>
2023-03-24 18:35:23 -0300

    Ajuste no resultado antes do retorno da API

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,27 +3,25 @@ import styles from './Main.module.css'
 
 export default function Main(){
     
-        const [resultado, setResultado] = useState('')
+        const [resultado, setResultado] = useState('Aguardando resultado')
         let cepDigitado = useRef('')
 
         function buscar(){
             fetch(`https://viacep.com.br/ws/${cepDigitado.current.value}/json/`)
             .then(retorno => retorno.json())
-            .then(dados => setResultado(dados))
+            .then(dados => setResultado(<h3>{dados.logradouro}, {dados.bairro} - {dados.localidade}/{dados.uf}</h3>))
             .catch(err => console.log('Erro no retorno da API'))
-            console.log(resultado)
         }
-    
+
     return(
         <div className={styles.containerNativo}>
             <h1>Busca CEP</h1>
             <div className={styles.containerElementos}>
-            <label htmlFor="input-cep">Digite um CEP válido:</label>
-            <input type="text" placeholder="Digite aqui" id="input-cep" name="input-cep" ref={cepDigitado} required></input>
-            <button type="submit" onClick={buscar}>Buscar</button>
-            <h3>{resultado.logradouro}, {resultado.bairro} - {resultado.localidade}/{resultado.uf}</h3>
-            </div>
-            
+                <label htmlFor="input-cep">Digite um CEP válido:</label>
+                <input type="text" placeholder="Digite aqui" id="input-cep" name="input-cep" ref={cepDigitado} required></input>
+                <button type="submit" onClick={buscar}>Buscar</button>
+                <h3>{resultado}</h3>
+            </div>    
         </div>
     )
 }
